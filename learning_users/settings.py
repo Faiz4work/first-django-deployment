e8e833a15bbcfe17c4954f8258ca20db4872f245
faiz ahmed <faizahmed11234@gmail.com>
2019-04-03 12:33:01 +0500

    add '127.0.0.1' to allowed host in settings.py file

--- learning_users/settings.py
+++ learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#+a0ioc$2_x8#ui3)o$mk2=14=mgdy(2^xbh7+841w*an(9vq9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.17']
+ALLOWED_HOSTS = ['192.168.0.17','127.0.0.1']
 
 
 # Application definition
